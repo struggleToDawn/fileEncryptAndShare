6ab12cd9bbb1746a4ce4839eab4504b2906ca55b
lilimin1234 <1148810694@qq.com>
2016-05-12 20:42:55 +0800

    fg

--- webSystem/sshdemo/src/main/java/pku/yang/controller/FreeGroupController.java
+++ webSystem/sshdemo/src/main/java/pku/yang/controller/FreeGroupController.java
@@ -251,7 +251,7 @@ public class FreeGroupController {
 		String fgfile_id=fgfileService.add_fgfile(fileid, folderid);
 		JSONObject result = new JSONObject();
 		result.put("code", 0);
-		result.put("data", "");
+		result.put("data", fgfile_id);
 		return result.toJSONString();	
 	}
 	
@@ -278,6 +278,7 @@ public class FreeGroupController {
 		if(uuid.endsWith(file_owner)||uuid.endsWith(fg_manager))
 		{
 			fileService.deleteFile(fileId);
+			fgfileService.delete_fgfile(fgfile_id);
 			result.put("code", 0);
 			result.put("data", "success");
 		}
@@ -293,7 +294,7 @@ public class FreeGroupController {
 	//下载文件token,fileid
 	//
 	@ResponseBody
-	@RequestMapping(value = "/downloadfile", method = RequestMethod.POST)
+	@RequestMapping(value = "/downloadfile", method = RequestMethod.GET)
 	public String downloadFile(@RequestParam String token,
 			@RequestParam String fgfile_id) {
 		JSONObject result = new JSONObject();
@@ -307,7 +308,8 @@ public class FreeGroupController {
 		FreegroupFile fgfile=fgfileService.search_fgfile_info(fgfile_id);
 		String fileId=fgfile.getFile_id();
 		result.put("code", 0);
-		result.put("data", "success");
+		result.put("file_id", fileId);
+		
 		return result.toJSONString();
 	}
 }
